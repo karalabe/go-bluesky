cd7fc0a72e9a39faba6ffb8690a5829e76a3affb
Péter Szilágyi <peterke@gmail.com>
2023-05-06 10:18:34 +0300

    Minor nitpicks to have a more uniform style

--- client_test.go
+++ client_test.go
@@ -15,6 +15,62 @@ import (
 	"github.com/bluesky-social/indigo/xrpc"
 )
 
+// testCredentials contains credentials from the environment to use for Client
+// login tests.
+type testCredentials struct {
+	handle string
+	passwd string
+	appkey string
+}
+
+// makeTestClient returns a Client and authentication credentials from env vars
+// that can be used to log in. The test will be skipped if the required variables
+// are not set.
+func makeTestClient(t *testing.T) (*Client, *testCredentials) {
+	t.Helper()
+
+	var (
+		handle = getenvOrSkip(t, "GOBLUESKY_TEST_HANDLE")
+		passwd = getenvOrSkip(t, "GOBLUESKY_TEST_PASSWD")
+		appkey = getenvOrSkip(t, "GOBLUESKY_TEST_APPKEY")
+	)
+	client, err := Dial(context.Background(), ServerBskySocial)
+	if err != nil {
+		t.Fatalf("failed to dial: %v", err)
+	}
+	t.Cleanup(func() { client.Close() })
+
+	return client, &testCredentials{
+		handle: handle,
+		passwd: passwd,
+		appkey: appkey,
+	}
+}
+
+// makeTestClientWithLogin returns a Client which is logged in using credentials
+// from the environment. The test will be skipped if the required env vars are
+// not set.
+func makeTestClientWithLogin(t *testing.T) *Client {
+	t.Helper()
+
+	client, creds := makeTestClient(t)
+	if err := client.Login(context.Background(), creds.handle, creds.appkey); err != nil {
+		t.Fatalf("failed to login to Bluesky server: %v", err)
+	}
+	return client
+}
+
+// getenvOrSkip fetches the value of env or skips the test if env is not set.
+func getenvOrSkip(t *testing.T, env string) string {
+	t.Helper()
+
+	val, ok := os.LookupEnv(env)
+	if !ok {
+		t.Skipf("skipping, environment variable %q is required to run the tests", env)
+	}
+	return val
+}
+
 // Basic test to see if connecting to a Bluesky instance works.
 func TestDial(t *testing.T) {
 	ctx := context.Background()
@@ -28,23 +84,23 @@ func TestDial(t *testing.T) {
 // Tests that logging into a Bluesky server works and also that only app passwords
 // are accepted, rejecting master credentials.
 func TestLogin(t *testing.T) {
-	client, creds := testClient(t)
+	client, creds := makeTestClient(t)
 	ctx := context.Background()
 
-	if err := client.Login(ctx, creds.Handle, "definitely-not-my-password"); !errors.Is(err, ErrLoginUnauthorized) {
+	if err := client.Login(ctx, creds.handle, "definitely-not-my-password"); !errors.Is(err, ErrLoginUnauthorized) {
 		t.Errorf("invalid password error mismatch: have %v, want %v", err, ErrLoginUnauthorized)
 	}
-	if err := client.Login(ctx, creds.Handle, creds.Passwd); !errors.Is(err, ErrLoginUnauthorized) || !errors.Is(err, ErrMasterCredentials) {
+	if err := client.Login(ctx, creds.handle, creds.passwd); !errors.Is(err, ErrLoginUnauthorized) || !errors.Is(err, ErrMasterCredentials) {
 		t.Errorf("master password error mismatch: have %v, want %v: %v", err, ErrLoginUnauthorized, ErrMasterCredentials)
 	}
-	if err := client.Login(ctx, creds.Handle, creds.Appkey); err != nil {
+	if err := client.Login(ctx, creds.handle, creds.appkey); err != nil {
 		t.Errorf("app password error mismatch: have %v, want %v", err, nil)
 	}
 }
 
 // Tests that the JWT token will not get refreshed if it's still valid.
 func TestJWTNoopRefresh(t *testing.T) {
-	client := testClientLoggedIn(t)
+	client := makeTestClientWithLogin(t)
 
 	errc := make(chan error, 1)
 	client.jwtRefreshHook = func(skip bool, async bool) {
@@ -63,7 +119,7 @@ func TestJWTNoopRefresh(t *testing.T) {
 // Tests that the JWT token can be refreshed async if the expiration time becomes
 // less than the allowed window.
 func TestJWTAsyncRefresh(t *testing.T) {
-	client := testClientLoggedIn(t)
+	client := makeTestClientWithLogin(t)
 
 	errc := make(chan error, 1)
 	client.jwtRefreshHook = func(skip bool, async bool) {
@@ -98,7 +154,7 @@ func TestJWTAsyncRefresh(t *testing.T) {
 // Tests that the JWT token will be refreshed sync if the expiration time becomes
 // less than the allowed window.
 func TestJWTSyncRefresh(t *testing.T) {
-	client := testClientLoggedIn(t)
+	client := makeTestClientWithLogin(t)
 
 	errc := make(chan error, 1)
 	client.jwtRefreshHook = func(skip bool, async bool) {
@@ -132,7 +188,7 @@ func TestJWTSyncRefresh(t *testing.T) {
 // Tests that if even the JWT refresh token got expired, teh refresher errors
 // out synchronously.
 func TestJWTExpiredRefresh(t *testing.T) {
-	client := testClientLoggedIn(t)
+	client := makeTestClientWithLogin(t)
 	client.jwtCurrentExpire = time.Time{}
 	client.jwtRefreshExpire = time.Time{}
 
@@ -144,7 +200,7 @@ func TestJWTExpiredRefresh(t *testing.T) {
 // Tests that the library can be used to do custom atproto calls directly if some
 // operation is not implemented.
 func TestCustomCall(t *testing.T) {
-	client := testClientLoggedIn(t)
+	client := makeTestClientWithLogin(t)
 	err := client.CustomCall(func(api *xrpc.Client) error {
 		_, err := atproto.ServerGetSession(context.Background(), api)
 		return err
@@ -152,63 +208,4 @@ func TestCustomCall(t *testing.T) {
 	if err != nil {
 		t.Fatalf("failed to execute custom call: %v", err)
 	}
-}
-
-// clientCredentials contains credentials from the environment to use for Client
-// login tests.
-type clientCredentials struct {
-	Handle, Passwd, Appkey string
-}
-
-// testClient returns a Client and credentials from the environment that can be
-// used to log in. The test will be skipped if the required environment
-// variables are not set.
-func testClient(t *testing.T) (*Client, *clientCredentials) {
-	t.Helper()
-
-	var (
-		handle = getenv(t, "GOBLUESKY_TEST_HANDLE")
-		passwd = getenv(t, "GOBLUESKY_TEST_PASSWD")
-		appkey = getenv(t, "GOBLUESKY_TEST_APPKEY")
-	)
-
-	ctx := context.Background()
-
-	client, err := Dial(ctx, ServerBskySocial)
-	if err != nil {
-		t.Fatalf("failed to dial: %v", err)
-	}
-	t.Cleanup(func() { client.Close() })
-
-	return client, &clientCredentials{
-		Handle: handle,
-		Passwd: passwd,
-		Appkey: appkey,
-	}
-}
-
-// testClientLoggedIn returns a Client which is logged in using credentials from
-// the environment. The test will be skipped if the required environment
-// variables are not set.
-func testClientLoggedIn(t *testing.T) *Client {
-	t.Helper()
-
-	client, creds := testClient(t)
-	if err := client.Login(context.Background(), creds.Handle, creds.Appkey); err != nil {
-		t.Fatalf("failed to login: %v", err)
-	}
-
-	return client
-}
-
-// getenv fetches the value of env or skips the test if env is not set.
-func getenv(t *testing.T, env string) string {
-	t.Helper()
-
-	s, ok := os.LookupEnv(env)
-	if !ok {
-		t.Skipf("skipping, environment variable %q is required to run the tests", env)
-	}
-
-	return s
 }
